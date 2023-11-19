d8379d67fd42a7f40849d3bc642946476b88bbda
Javier Raell <alvnaltair@gmail.com>
2023-11-19 20:38:08 +0700

    coba

---
+++ GerejaUAS/resources/views/components/hero.blade.php
@@ -0,0 +1,8 @@
+<div class='hero flex d-flex flex-column justify-content-center align-items-center'>
+  <img src="images/hero.jpg" alt="foto gereja">  
+  <h1 class='text-center fw-bold'>Gereja Modern di <br> pusat jakarta</h1>
+
+  <button class='btn btn-primary mt-3 p-4'>
+    <a href='/jadwal' class='fw-bold'> Aktivitas Kami</q>
+  </button>
+</div>
---
+++ GerejaUAS/resources/views/pages/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+
+@section('content')
+
+  @include('components.navbar')
+  @include('components.hero')
+@endsection
