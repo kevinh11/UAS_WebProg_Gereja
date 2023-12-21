97f0ea0e606792956fc5e7d0dd322fbab8548e8f
gakukek <darrylclaylayaya@gmail.com>
2023-12-21 14:38:37 +0700

    index for web hosting

--- Gereja/config/app.php
+++ Gereja/config/app.php
@@ -169,7 +169,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Intervention\Image\ImageManager::class,
+        //Intervention\Image\ImageManager::class,
         
     ])->toArray(),
 
