e3e909f97d6f352aad92b1ecd3aaf3789ad616db
gakukek <darrylclaylayaya@gmail.com>
2023-12-22 17:21:00 +0700

    prelude to galeri

--- Gereja/app/Http/Controllers/GaleriController.php
+++ Gereja/app/Http/Controllers/GaleriController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Image;
 
 class GaleriController extends Controller
 {
     function index(){
-        return view('pages.galeri');
+        $this->read();
+        return view('pages.galeri', ['galeri'=>$this->galeri]);
     }
+
+    public $galeri = [];
+
+    function read(){
+        $this->galeri = Image::all();
+    }
+
 }
