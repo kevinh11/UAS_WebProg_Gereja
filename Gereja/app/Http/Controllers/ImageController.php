152e39d700f9fa41b3b80146845ae31c9d294e06
gakukek <darrylclaylayaya@gmail.com>
2023-12-22 22:54:16 +0700

    last change before 2nd production ocmmit

--- Gereja/app/Http/Controllers/ImageController.php
+++ Gereja/app/Http/Controllers/ImageController.php
@@ -4,55 +4,53 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Image;
-use Intervention\Image\ImageManager;
+use Intervention\Image\Image as InterventionImage;
 use Intervention\Image\Drivers\Gd\Driver;
-
+use Intervention\Image\ImageManager;
+use Illuminate\Http\UploadedFile;
+use Intervention\Image\Encoders\AutoEncoder;
 class ImageController extends Controller
 {
     private $images = [];
 
-    function read()
-    {
+    function read() {
         $this->images = Image::all()->toArray();
     }
 
-    function edit($id, Request $req)
-    {
+    function edit($id, Request $req) {
 
         $image = Image::find($id);
-        $img = $req->file('img');
+        $img = $req->file('gambar');
         $img_name = $img->getClientOriginalName();
 
         $path = $req->input('path');
         $desc = $req->input('desc');
 
         if ($this->verify_ext($img_name) && $img->isValid()) {
-            $manager = new ImageManager(new Driver());
-
-
-
-            $image->img = $img;
+            $manager = ImageManager::withDriver(new Driver());
+            $image->img = $this->compressImage($img);
             $image->path = $path;
             $image->desc = $desc;
             $image->save();
             return redirect('admin-dashboard');
-        } else {
+        }
+
+        else {
             return redirect()->back()->with('success', 'Gambar tidak bisa di save. Pastikan file berupa gambar');
         }
+        
     }
 
-    function delete($id)
-    {
+    function delete($id) {
         Image::find($id)->delete();
         return redirect('admin-dashboard');
     }
 
 
-    function verify_ext($file_name)
-    {
+    function verify_ext($file_name) {
         $ext = pathinfo($file_name, PATHINFO_EXTENSION);
 
-        switch ($ext) {
+        switch($ext) {
             case 'gif':
             case 'png':
             case 'jpg':
@@ -64,42 +62,49 @@ class ImageController extends Controller
         }
     }
 
-    function create(Request $req)
-    {
+    function create(Request $req) {
 
-        $img = Image::make($req->file('gambar'));
+        $img = $req->file('gambar');
         $desc = $req->input('desc');
         $img_name = $img->getClientOriginalName();
+        $manager = new ImageManager(new Driver());
+        $image = $manager->read($img);
 
-        $compressedImageData = $this->compressImage($img);
+        $compressedImageData = $this->compressImage($image);
 
         if ($this->verify_ext($img_name) && $img->isValid()) {
-
-            $manager = new ImageManager(new Driver());
-
-            Image::create([
-                'img' => file_get_contents($compressedImageData->getRealPath()),
-                'desc' => $desc
+           Image::create([
+                'img'=>$compressedImageData,
+                'desc'=>$desc
             ]);
             return redirect('admin-dashboard');
-        } else {
+        }
+        else {
             return redirect()->back()->with('success', 'Gambar tidak bisa di save. Pastikan file berupa gambar');
         }
+        
+
     }
 
-    function display_edit_view($id)
-    {
+    function compressImage($image) {
+        $manager = new ImageManager(new Driver());
+        $compressedImage = $manager->read($image);
+        $compressedImageData = $compressedImage->toJpeg(90);
+
+        return $compressedImageData;
+    }
+
+    function display_edit_view($id) {
         $currImage = Image::find($id)->toArray();
-        return view('pages.admin_form.edit_image', ['image' => $currImage]);
+        return view('pages.admin_form.edit_image', ['image'=> $currImage]);
     }
 
-    function display_create_view()
-    {
+    function display_create_view() {
         return view('pages.admin_form.add_image');
     }
-    function index()
-    {
+    function index() {
         $this->read();
-        return view('pages.galeri', ['images' => $this->images]);
+        return view('pages.galeri', ['images'=>$this->images]);
+
     }
 }
